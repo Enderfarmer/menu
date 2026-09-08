4e7e0a573a964ef65fb6e9006ddd02fef846a781
Enderfarmer <150594153+Enderfarmer@users.noreply.github.com>
2026-09-08 15:48:41 +0200

    Simplify button position logic on mouseenter

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 let btn = document.getElementById("catchme")
 let counter = 0
 btn.addEventListener("mouseenter", () => {
-  if (btn.style.position != "absolute") {
-    btn.style.position = "absolute";
+  btn.style.position = "absolute";
     counter = (counter + 1) % 5;
   let x = counter * 200 + 100;
   btn.style.left = x + "px"
-  }
 }
 )
 btn.addEventListener("click", () => {alert("You did it!")});
