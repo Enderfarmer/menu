7a60d0888472b304a0910ef1c63166330501b120
Enderfarmer <150594153+Enderfarmer@users.noreply.github.com>
2026-09-08 15:44:27 +0200

    Update button behavior on mouseenter event
    Change button position to absolute on mouseenter and update its left position based on the counter.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let btn = document.getElementById("catchme")
 let counter = 0
 btn.addEventListener("mouseenter", () => {
-  counter = (counter + 1) % 5;
+  if (btn.style.position != "absolute") {
+    btn.style.position = "absolute";
+    counter = (counter + 1) % 5;
   let x = counter * 200 + 100;
   btn.style.left = x + "px"
+  }
 }
 )
 btn.addEventListener("click", () => {alert("You did it!")});
