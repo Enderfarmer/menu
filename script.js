bd5aad76b30378a0a76d825e023192cdbe3d3644
Enderfarmer <150594153+Enderfarmer@users.noreply.github.com>
2026-09-08 15:40:04 +0200

    Refactor button position calculation and add loop
    Refactor variable declaration for 'x' and add infinite loop to update button position.

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ let btn = document.getElementById("catchme")
 let counter = 0
 btn.addEventListener("mouseenter", () => {
   counter = (counter + 1) % 5;
-  x = counter * 200 + 100;
+  let x = counter * 200 + 100;
   btn.style.left = x + "px"
 }
 )
 btn.addEventListener("click", () => {alert("You did it!")});
+while (true) {
+  let x = counter * 200 + 100;
+  btn.style.left = x + "px"
+  btn.style.position = "absolute !important";
+}
